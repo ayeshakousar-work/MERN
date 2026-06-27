ff601e5e46afb2826ebc695b2948d25ff31d4d78
Ayesha Kousar <ayeshakousar.work@gmail.com>
2026-06-27 14:53:04 +0500

    Update leagues.jsx

--- frontend/src/Pages/user/leagues.jsx
+++ frontend/src/Pages/user/leagues.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; 
 import './league.css';
-const BASE_URL = 'http://localhost:5000';
-
+const BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
 const Leagues = () => {
   const [leagues, setLeagues] = useState([]);
   const navigate = useNavigate();
