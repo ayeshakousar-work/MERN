e13c4743cb7813f52fb01d66a39f24db16dd3b5b
Ayesha Kousar <ayeshakousar.work@gmail.com>
2026-06-27 14:47:36 +0500

    Update AdminPanel.jsx

--- frontend/src/Pages/admin/AdminPanel.jsx
+++ frontend/src/Pages/admin/AdminPanel.jsx
@@ -7,7 +7,7 @@ export default function Panel() {
   const [adminInfo, setAdminInfo] = useState({ name: '', image: '' });
   const [loading, setLoading] = useState(true);
 
-  const BASE_URL = 'http://localhost:5000';
+  const BASE_URL =  process.env.REACT_APP_API_URL || "http://localhost:5000";
   const navigate = useNavigate();
 
   useEffect(() => {
